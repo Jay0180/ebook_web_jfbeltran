a32a9996d3005dd4e05ff6793460e542ed31f0ff
Jay0180 <jfbeltran@sscgi.com>
2025-02-25 17:24:20 +0800

    Added Navbar transition

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ function userScroll() {
     const navbar = document.querySelector('.navbar');
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY) {
+        if (window.scrollY > 50) {
             navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky');
         }
         else {
             navbar.classList.remove('bg-dark');
+            navbar.classList.remove('navbar-sticky');
         }
     });
 }
